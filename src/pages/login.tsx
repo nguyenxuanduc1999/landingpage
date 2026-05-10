d59dee9b4b8ccf536d84986f49a133cf052d7819
Xuan Duc Nguyen <nguyenxuanduc1999@gmail.com>
2026-05-10 09:59:43 +0200

    fix: edit login page to fit display

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,11 +2,16 @@ import { AuthComponent } from "@/components/ui/sign-up";
 
 export default function CustomAuthDemo() {
   return (
-    <div className="bg-white min-h-screen text-black">
-      <AuthComponent
-        logo={<img src="./public/logoXDN.png" alt="Logo" className="h-20 w-20" />}
-        brandName=""
-      />
-    </div>
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <div className="bg-white min-h-screen text-black">
+        <AuthComponent
+          logo={<img src="./public/logoXDN.png" alt="Logo" className="h-20 w-20" />}
+          brandName=""
+        />
+      </div>
+    </>
   );
 }
